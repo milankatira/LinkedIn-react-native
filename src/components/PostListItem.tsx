1fd2d65534dbacab1511f457a69a323e6c21ad55
milankatira26@gmail.com <milankatira26@gmail.com>
2023-07-28 19:49:19 +0530

    added user profile

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -24,16 +24,20 @@ const PostListItem = ({ post }: PostListItemProps) => {
     <Link href={`/posts/${post.id}`} asChild>
       <Pressable style={styles.container}>
         <View style={styles.container}>
-          <View style={styles.header}>
-            <Image
-              source={{ uri: post.author.image }}
-              style={styles.userImage}
-            />
-            <View>
-              <Text style={styles.userName}>{post.author.name}</Text>
-              <Text style={styles.position}>{post.author.position}</Text>
-            </View>
-          </View>
+          <Link href={`/users/${post.author.id}`} asChild>
+            <Pressable>
+              <View style={styles.header}>
+                <Image
+                  source={{ uri: post.author.image }}
+                  style={styles.userImage}
+                />
+                <View>
+                  <Text style={styles.userName}>{post.author.name}</Text>
+                  <Text style={styles.position}>{post.author.position}</Text>
+                </View>
+              </View>
+            </Pressable>
+          </Link>
 
           <Text style={styles.content}>{post.content}</Text>
           {post.image && (
--- src/types.ts
+++ src/types.ts
@@ -11,4 +11,12 @@ export type User = {
   name: string;
   position: string;
   image?: string;
+  experience?: Experience[];
+};
+
+export type Experience = {
+  id: string;
+  title: string;
+  companyName: string;
+  companyImage: string;
 };
